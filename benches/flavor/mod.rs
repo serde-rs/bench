1210ed76bc6c666d23424b6dacbe32287728bcf2
David Tolnay <dtolnay@gmail.com>
2025-12-27 13:05:27 -0800

    Add postcard2

--- benches/flavor/mod.rs
+++ benches/flavor/mod.rs
@@ -1,5 +1,4 @@
-use postcard::ser_flavors::Flavor;
-use postcard::Result;
+use std::convert::Infallible;
 
 pub struct PreallocatedVec<'a> {
     vec: &'a mut Vec<u8>,
@@ -11,23 +10,46 @@ impl<'a> PreallocatedVec<'a> {
     }
 }
 
-impl<'a> Flavor for PreallocatedVec<'a> {
+impl<'a> postcard::ser_flavors::Flavor for PreallocatedVec<'a> {
     type Output = ();
 
     #[inline]
-    fn try_extend(&mut self, data: &[u8]) -> Result<()> {
+    fn try_extend(&mut self, data: &[u8]) -> postcard::Result<()> {
         self.vec.extend_from_slice(data);
         Ok(())
     }
 
     #[inline]
-    fn try_push(&mut self, data: u8) -> Result<()> {
+    fn try_push(&mut self, data: u8) -> postcard::Result<()> {
         self.vec.push(data);
         Ok(())
     }
 
     #[inline]
-    fn finalize(self) -> Result<Self::Output> {
+    fn finalize(self) -> postcard::Result<Self::Output> {
+        Ok(())
+    }
+}
+
+impl<'a> postcard2::ser_flavors::Flavor for PreallocatedVec<'a> {
+    type Output = ();
+    type PushError = Infallible;
+    type FinalizeError = Infallible;
+
+    #[inline]
+    fn try_extend(&mut self, data: &[u8]) -> Result<(), Infallible> {
+        self.vec.extend_from_slice(data);
+        Ok(())
+    }
+
+    #[inline]
+    fn try_push(&mut self, data: u8) -> Result<(), Infallible> {
+        self.vec.push(data);
+        Ok(())
+    }
+
+    #[inline]
+    fn finalize(self) -> Result<Self::Output, Infallible> {
         Ok(())
     }
 }
